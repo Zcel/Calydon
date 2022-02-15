3370593e99b3e8cbc60393ca818835acf4d059a2
Julien VOS <vosjulien10@gmail.com>
2022-02-15 21:01:02 +0100

    Games page

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function onClickSMP(){
+    document.getElementById('smp').style.display='';
+    document.getElementById('duels').style.cssText='display: none;';
+}
+
+function onClickDuels(){
+    document.getElementById('duels').style.display='';
+    document.getElementById('smp').style.cssText='display: none;';
+}
